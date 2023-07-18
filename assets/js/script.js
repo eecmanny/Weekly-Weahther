e4c541bd8f728c6fbdd01f0e8d67e74e1ddbf559
eecmanny <eec123manny@gmail.com>
2023-07-18 06:04:35 -0400

    html

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+var geocodeURL = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=ac7cbe64d8841fa5ae6093c5853d6268'
+var weatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=ac7cbe64d8841fa5ae6093c5853d6268'
+var movieUrl = 'https://api.themoviedb.org/3/movie/now_playing'
+
+
+var country = "US"
+var state = "";
+var town = "";
+
