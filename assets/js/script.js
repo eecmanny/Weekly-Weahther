795838f9db064871a55572ad96add2f50b4ba481
eecmanny <130661353+eecmanny@users.noreply.github.com>
2023-07-19 14:35:39 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
 
 var geocodeURL = 'https://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=ac7cbe64d8841fa5ae6093c5853d6268'
-var weatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=ac7cbe64d8841fa5ae6093c5853d6268'
+var forcastWeatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=ac7cbe64d8841fa5ae6093c5853d6268'
+var currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=ac7cbe64d8841fa5ae6093c5853d6268"
 var movieUrl = 'https://api.themoviedb.org/3/movie/now_playing'
+var country = "US"
+var state = document.querySelector(".state"); "";
+var town = document.querySelector("#");= "";
 
 
 var todayWeather = document.querySelector(".currentWeather");
@@ -12,9 +16,6 @@ var weatherDay4 = document.querySelector(".Day4");
 var weatherDay5 = document.querySelector(".Day5");
 
 
-var country = "US"
-var state = "";
-var town = "";
 
 function weatherFetching() {
   if (navigator.geolocation) {
@@ -24,23 +25,40 @@ function weatherFetching() {
 //Geo API
   //This function should fetch a latitude and longitude by adding a country, state, and town 
   function success(position) {
-    // var lat = position.coords.latitude;
-    // var lon = position.coords.longitude;
+var state = "";
+var town = "";
 
     fetch("https://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=ac7cbe64d8841fa5ae6093c5853d6268")
         .then(response => {
           return response.json();
-
-
-  //Weather API
+          };
+  
+          
+  //forcastWeather API
     //This function should fetch grabs the fetched latitude and longitude and fetched the data for that days weather. 
   function success(position) {
     // var lat = position.coords.latitude;
     // var lon = position.coords.longitude;
-
+  
     //Using browser location
-    var lat = GeoLatplaceholder;
-    var lon = GeoLonplaceholder;
+    // var lat = GeoLatplaceholder;
+    // var lon = GeoLonplaceholder;
+    fetch('https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=ac7cbe64d8841fa5ae6093c5853d6268')
+        .then(response => {
+          return response.json();
+        };
+
+          
+  //CurrentWeather API
+    //This function should fetch grabs the fetched latitude and longitude and fetched the data for that days weather. 
+  function success(position) {
+    // var lat = position.coords.latitude;
+    // var lon = position.coords.longitude;
+  
+    //Using browser location
+    // var lat = GeoLatplaceholder;
+    // var lon = GeoLonplaceholder;
     fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=ac7cbe64d8841fa5ae6093c5853d6268")
         .then(response => {
           return response.json();
+        };
