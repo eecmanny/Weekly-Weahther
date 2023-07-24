60c51604be694cf26aab5d9f281672298ca1562b
eecmanny <eec123manny@gmail.com>
2023-07-24 06:01:50 -0400

    api working

--- assets/js/Basic-script.js
+++ assets/js/Basic-script.js
@@ -1,23 +1,7 @@
-var variableGeocodeURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + textPickedCity +",US&limit=5&appid=ac7cbe64d8841fa5ae6093c5853d6268"
-// var geocodeURL = "https://api.openweathermap.org/geo/1.0/direct?q=Durham,US&limit=5&appid=ac7cbe64d8841fa5ae6093c5853d6268"
-
-
-var variableCurrentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=ac7cbe64d8841fa5ae6093c5853d6268"
-// var currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat=41.4817647&lon=-72.6812059&appid=ac7cbe64d8841fa5ae6093c5853d6268"
-
-
-var variableForcastWeatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=ac7cbe64d8841fa5ae6093c5853d6268"
-// var forcastWeatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=41.4817647&lon=-72.6812059&appid=ac7cbe64d8841fa5ae6093c5853d6268'
-
 var country = "US"
 
 //var city = document.querySelector("#city");
 var city = document.getElementById("city");
-var textPickedCity = [];
-console.log(textPickedCity);
-
-var lat = [];
-var lon = [];
 
 
 var todayWeather = document.querySelector(".currentWeather");
@@ -31,83 +15,84 @@ var submit = document.querySelector(".btn");
 
 
 document.addEventListener("DOMContentLoaded", function () {
-submit = addEventListener("click", chooseLocation);
+  submit = addEventListener("submit", chooseLocation);
 
-function chooseLocation(){
-  // event.preventDefault();
+  function chooseLocation(event) {
+    event.preventDefault();
 
-  // console.log(document.getElementById("city").value);
-  selectedCity = document.getElementById("city").value;
-  textPickedCity.push(selectedCity);
+    // console.log(document.getElementById("city").value);
+    const selectedCity = document.getElementById("city").value;
+    // textPickedCity.push(selectedCity);
+
+    // Console logging the selected city
+    console.log(selectedCity);
+
+    //Pass the selected City to your function
+    LatandLonFetching(selectedCity);
 
 
-  console.log(selectedCity);
-
-  LatandLonFetching();
-}
+  }
 });
 
 
 // LatandLonFetching();
 
-function LatandLonFetching() {
-
-  fetch(variableGeocodeURL)
-  // fetch(geocodeURL)
+function LatandLonFetching(city) {
+  //console.log the city to make sure it was passed correctly
+  console.log(city);
+  fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + city + ",US&limit=5&appid=ac7cbe64d8841fa5ae6093c5853d6268")
+    // fetch(geocodeURL)
     .then(response => {
       console.log(response);
       return response.json();
     })
     .then(function (data) {
       console.log(data)
+      // console.log(data[0].lat)
+      // console.log(data[0].lon)
 
+      var lat = data[0].lat;
+      var lon = data[0].lon;
+      console.log(lat);
+      console.log(lon);
+
+      forcastWeather(lat, lon);
+      currentWeather(lat, lon);
     })
-  forcastWeather();
-  currentWeather();
 };
 
 
-// // currentWeather();
 // // // CurrentWeather API
 // // // This function should fetch grabs the fetched latitude and longitude and fetched the data for that days weather. 
-// function currentWeather() {
-// //   // var lat = position.coords.latitude;
-// //   // var lon = position.coords.longitude;
-// fetch(variableCurrentWeatherUrl)
-//   // fetch(currentWeatherUrl)
-//     .then(response => {
-//       console.log(response);
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data)
-//     });
-// }
+function currentWeather(lat, lon) {
+//   // var lat = position.coords.latitude;
+//   // var lon = position.coords.longitude;
+fetch( "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=ac7cbe64d8841fa5ae6093c5853d6268")
+  // fetch(currentWeatherUrl)
+    .then(response => {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data)
+    });
+}
 
 // // //forcastWeather API
 // // //This function should fetch grabs the fetched latitude and longitude and fetched the data for that days weather. 
-// function forcastWeather() {
+function forcastWeather(lat, lon) {
 // //   // //   // var lat = position.coords.latitude;
 // //   // //   // var lon = position.coords.longitude;
 
-// fetch(variableForcastWeatherUrl)
-//   // fetch(forcastWeatherUrl)
-//     .then(response => {
-//       // console.log(response);
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       // console.log(data)
-//     })
-// };
+fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=ac7cbe64d8841fa5ae6093c5853d6268")
+  // fetch(forcastWeatherUrl)
+    .then(response => {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data)
+    })
+};
 
-
-  //   //Using browser location
-  //   // var lat = GeoLatplaceholder;
-  //   // var lon = GeoLonplaceholder;
-  //   fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=ac7cbe64d8841fa5ae6093c5853d6268")
-  //       .then(response => {
-  //         return response.json();
-  //       });
-  //     }
 
