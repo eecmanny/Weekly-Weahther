4994bb4da129af885b9f99306a30bd49f3f8f04a
Emmanuel Cordero <eec123manny@gmail.com>
2023-07-25 06:04:04 -0400

    allwatherAPI_data

--- assets/js/Basic-script.js
+++ assets/js/Basic-script.js
@@ -1,5 +1,9 @@
 var country = "US"
 
+var currentDate = dayjs();
+$("#currentDay").text(currentDate.format('dddd, MMMM D YYYY, h:mm:ss a'));
+console.log(currentDate);
+
 //var city = document.querySelector("#city");
 var city = document.getElementById("city");
 
@@ -65,33 +69,89 @@ function LatandLonFetching(city) {
 // // // CurrentWeather API
 // // // This function should fetch grabs the fetched latitude and longitude and fetched the data for that days weather. 
 function currentWeather(lat, lon) {
-//   // var lat = position.coords.latitude;
-//   // var lon = position.coords.longitude;
-fetch( "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=ac7cbe64d8841fa5ae6093c5853d6268")
-  // fetch(currentWeatherUrl)
+  //   // var lat = position.coords.latitude;
+  //   // var lon = position.coords.longitude;
+  fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=ac7cbe64d8841fa5ae6093c5853d6268")
+    // fetch(currentWeatherUrl)
     .then(response => {
-      console.log(response);
+      // console.log(response);
       return response.json();
     })
     .then(function (data) {
-      console.log(data)
+      // console.log(data)
+
+      // console.log(data.main);
+
+
+      // var temp = data.main.temp;
+      // console.log(data.main.temp);
+      // console.log(temp);
+
+
+      // var humidity = data.main.humidity;
+      // console.log(humidity);
+
+
+      // var wind = data.wind.speed;
+      // console.log(wind);
+
     });
 }
 
 // // //forcastWeather API
 // // //This function should fetch grabs the fetched latitude and longitude and fetched the data for that days weather. 
 function forcastWeather(lat, lon) {
-// //   // //   // var lat = position.coords.latitude;
-// //   // //   // var lon = position.coords.longitude;
+  // //   // //   // var lat = position.coords.latitude;
+  // //   // //   // var lon = position.coords.longitude;
 
-fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=ac7cbe64d8841fa5ae6093c5853d6268")
-  // fetch(forcastWeatherUrl)
+  fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=ac7cbe64d8841fa5ae6093c5853d6268")
+    // fetch(forcastWeatherUrl)
     .then(response => {
       console.log(response);
       return response.json();
     })
     .then(function (data) {
-      console.log(data)
+      console.log(data);
+
+      //Day 1
+      var Day1Temp = data.list[0].main.temp;
+      // console.log(data.list[0].main.temp);
+      // console.log(Day1Temp);
+
+
+      var Day1humidity = data.list[0].main.humidity;
+      // console.log(Day1humidity);
+
+      var Day1wind = data.list[0].wind.speed;
+      // console.log(Day1wind);
+      var dateNum1 = data.list[0].wind.dt;
+
+      //Day2
+      var Day2Temp = data.list[1].main.temp;
+      var Day2humidity = data.list[1].main.humidity;
+      var Day2wind = data.list[1].wind.speed;
+      var Day2dateNum = data.list[1].wind.dt;
+
+      //Day3
+      var Day3Temp = data.list[2].main.temp;
+      var Day3humidity = data.list[2].main.humidity;
+      var Day3wind = data.list[2].wind.speed;
+      var Day3dateNum = data.list[2].wind.dt;
+
+      //Day4
+      var Day4Temp = data.list[3].main.temp;
+      var Day4humidity = data.list[3].main.humidity;
+      var Day4wind = data.list[3].wind.speed;
+      var Day4dateNum = data.list[3].wind.dt;
+
+
+
+      //Day5
+      var Day5Temp = data.list[4].main.temp;
+      var Day5humidity = data.list[4].main.humidity;
+      var Day5wind = data.list[4].wind.speed;
+      var Day5dateNum = data.list[4].wind.dt;
+
     })
 };
 
